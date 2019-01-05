e067ac48e3ea0e5b1c4f0c3cc51a12b6167f7255
jeffrey.wang <wjb19830511@126.com>
2019-01-06 00:07:53 +0800

    add boyguard path emulation

--- manager/chess_manager.py
+++ manager/chess_manager.py
@@ -9,6 +9,7 @@ from chess_piece import *
 from chess_piece_soldier import *
 from chess_piece_vehicle import *
 from chess_piece_prince import *
+from chess_piece_bodyguard import *
 from util import *
 
 
@@ -81,9 +82,9 @@ class ChessManager:
         self.chess_piece_list.append(ChessPieceVehicle(0, 0, 0, 0))  # 0, 0, '车', green
         self.chess_piece_list.append(ChessPiece(0, 1, 1, 0))  # 0, 1, '马', green
         self.chess_piece_list.append(ChessPiece(0, 2, 3, 0))  # 0, 2, '象', green
-        self.chess_piece_list.append(ChessPiece(0, 3, 4, 0))  # 0, 3, '士', green
+        self.chess_piece_list.append(ChessPieceBoyguard(0, 3, 4, 0))  # 0, 3, '士', green
         self.chess_piece_list.append(ChessPiecePrince(0, 4, 5, 0))  # 0, 4, '将'  green
-        self.chess_piece_list.append(ChessPiece(0, 5, 4, 0))  # 0, 5, '士', green
+        self.chess_piece_list.append(ChessPieceBoyguard(0, 5, 4, 0))  # 0, 5, '士', green
         self.chess_piece_list.append(ChessPiece(0, 6, 3, 0))  # 0, 6, '象', green
         self.chess_piece_list.append(ChessPiece(0, 7, 1, 0))  # 0, 7, '马', green
         self.chess_piece_list.append(ChessPieceVehicle(0, 8, 0, 0))  # 0, 8, '车', green
@@ -98,9 +99,9 @@ class ChessManager:
         self.chess_piece_list.append(ChessPieceVehicle(9, 0, 0, 1))  # 0, 0, '车', red
         self.chess_piece_list.append(ChessPiece(9, 1, 1, 1))  # 0, 1, '马', red
         self.chess_piece_list.append(ChessPiece(9, 2, 3, 1))  # 0, 2, '象', red
-        self.chess_piece_list.append(ChessPiece(9, 3, 4, 1))  # 0, 3, '士', red
+        self.chess_piece_list.append(ChessPieceBoyguard(9, 3, 4, 1))  # 0, 3, '士', red
         self.chess_piece_list.append(ChessPiecePrince(9, 4, 5, 1))  # 0, 4, '将'  red
-        self.chess_piece_list.append(ChessPiece(9, 5, 4, 1))  # 0, 5, '士', red
+        self.chess_piece_list.append(ChessPieceBoyguard(9, 5, 4, 1))  # 0, 5, '士', red
         self.chess_piece_list.append(ChessPiece(9, 6, 3, 1))  # 0, 6, '象', red
         self.chess_piece_list.append(ChessPiece(9, 7, 1, 1))  # 0, 7, '马', red
         self.chess_piece_list.append(ChessPieceVehicle(9, 8, 0, 1))  # 0, 8, '车', red
---
+++ manager/chess_piece_bodyguard.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 1/5/2019 11:35 PM
+# @Author  : 王金波
+# @File    : chess_piece_bodyguard.py
+
+
+from util import *
+from chess_piece import ChessPiece
+
+class ChessPieceBoyguard(ChessPiece):
+
+    '''
+        desc: 将当前棋子随机移动。（不同category棋子移动规律与范围不同）。穷举法：
+        对方
+        (0,3)->(1,4)
+        (0,5)->(1,4)
+        (2,3)->(1,4)
+        (2,5)->(1,4)
+        (1,4)->(0,3)
+        (1,4)->(0,5)
+        (1,4)->(2,3)
+        (1,4)->(2,5)
+        己方
+        (9,3)->(8,4)
+        (9,5)->(8,4)
+        (7,3)->(8,4)
+        (7,5)->(8,4)
+        (8,4)->(9,3)
+        (8,4)->(9,5)
+        (8,4)->(7,3)
+        (8,4)->(7,5)
+    '''
+    def get_next_position_list(self, chess_manager):
+        # 士
+        next_pos_list = []  # [(row1, col1), (row2, col2), ...]
+        if self.type == 1 and 7 <= self.row <= 9 and 3 <= self.col <= 5:  # 己方棋子
+            if (((self.row == 9 and self.col == 3)
+                    or (self.row == 9 and self.col == 5)
+                    or (self.row == 7 and self.col == 3)
+                    or (self.row == 7 and self.col == 5))
+                    and (self.type != chess_manager.get_type_at_pos(8, 4))):
+                position = (8, 4)
+                next_pos_list.append(position)
+            if self.row == 8 and self.col == 4:
+                if self.type != chess_manager.get_type_at_pos(9, 3):
+                    position = (9, 3)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(9, 5):
+                    position = (9, 5)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(7, 3):
+                    position = (7, 3)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(7, 5):
+                    position = (7, 5)
+                    next_pos_list.append(position)
+        elif self.type == 0 and 0 <= self.row <= 2 and 3 <= self.col <= 5:  # 对方棋子
+            if (((self.row == 0 and self.col == 3)
+                    or (self.row == 0 and self.col == 5)
+                    or (self.row == 2 and self.col == 3)
+                    or (self.row == 2 and self.col == 5))
+                    and (self.type != chess_manager.get_type_at_pos(1, 4))):
+                position = (1, 4)
+                next_pos_list.append(position)
+            if self.row == 1 and self.col == 4:
+                if self.type != chess_manager.get_type_at_pos(0, 3):
+                    position = (0, 3)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(0, 5):
+                    position = (0, 5)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(2, 3):
+                    position = (2, 3)
+                    next_pos_list.append(position)
+                if self.type != chess_manager.get_type_at_pos(2, 5):
+                    position = (2, 5)
+                    next_pos_list.append(position)
+
+        # elif self.type == 0 and 0 <= self.row <= 2 and 3 <= self.col <= 5:  # 对方棋子
+        #     # 竖着走：
+        #     self.update_go_vertical_position(next_pos_list, north_type, south_type)
+        #     # 横着走:
+        #     self.update_go_horizontal_position(left_type, next_pos_list, right_type)
+        return next_pos_list
